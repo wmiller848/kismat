936d4071331b9e41dbd44d426b8d95cfac7d2ae5
William C Miller <wmillerx@gmail.com>
2015-03-15 10:29:40 -0700

    stuff

--- main.go
+++ main.go
@@ -1,41 +1,38 @@
-//
-//
+
+// The MIT License (MIT)
+
+// Copyright (c) 2014 William Miller
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 package main
 
 import (
 	// Standard
 	"fmt"
-	"net"
-	_ "time"
-	// 3rd Party
-	_ "code.google.com/p/gopacket"
-	"code.google.com/p/gopacket/pcap"
-	// Kismat
+  // 3rd Party
+  // Kismat
+  "github.com/wmiller848/kismat/lib"
 )
 
 func main() {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Printf("%+v\r\n", ifaces)
-	handle, err := pcap.OpenLive("lo0", 1024, true, pcap.BlockForever)
-        if err != nil {
-		fmt.Println(err.Error())
-		return
-        }
-	err = handle.SetBPFFilter("tcp")
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-        }
-	//packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-  	//fmt.Println(packetSource)
-	//for packet := range packetSource.Packets() {
-  	//	fmt.Printf("%+v\r\n", packet)
-	//}
-	data, info, err := handle.ReadPacketData()
-	fmt.Println(data, info, err)
+  fmt.Println("Starting Kismat")
+	kis := kismat.NewKismat()
+  kis.Bind()
 }
